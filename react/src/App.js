7621a72f5a2cfec5deec5d9bdc70e2d67a9037c6
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2022-03-31 22:24:51 +0700

    add surprise source code
    a little surprise site for my wife that can be accessed through our intranet.

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+        <h3 className='header'>Hai sayang.....</h3>
+        <h3 className='header'>Kamu cantik banget hari ini...</h3>
+        <h3 className='header'>I love you 😘 😘 😘</h3>
+        <img src="public/../heart.png" width="150" height="150"/>
+    </div>
+  );
+}
+
+export default App;
